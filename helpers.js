13f4692eaf51d814420b4ca8a4144fc48b0f1bc2
JgutierrezG <bolmack@hotmail.com>
2019-11-20 17:41:17 -0600

    Commented helper.txt

---
+++ helpers.js
@@ -0,0 +1,23 @@
+//Added comments
+const add = (number1, number2) => {
+  return 1;
+}
+
+const divide = (number1, number2) => {
+  return 1;
+}
+
+const multiply = (number1, number2) => {
+  return 1;
+}
+
+const substract = (number1, number2) => {
+  return 1;
+}
+
+module.exports = {
+  add,
+  divide,
+  multiply,
+  substract,
+};
